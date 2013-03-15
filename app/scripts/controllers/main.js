c38fb69a25c48ef6059a4d67aff7075796b41bcd
John Long <nakaroka@gmail.com>
2013-03-15 11:15:08 -0600

    first commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('trainingApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Testacular'
+    ];
+  });
